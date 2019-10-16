64dda579248dd23921d50f26b9b695fb9929fe8e
Łukasz Czepielik <koragrex@gmail.com>
2019-10-16 20:00:30 +0200

    Added RegexPatterns. Rebuilded FileLineRegexRecognizer project and added mixedFormats.txt.

--- FileLineRegexRecognizer/Program.cs
+++ FileLineRegexRecognizer/Program.cs
@@ -1,6 +1,7 @@
 ﻿using FileLineRegexRecognizer.Model;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Text.RegularExpressions;
 
@@ -8,15 +9,38 @@ namespace FileLineRegexRecognizer
 {
     class Program
     {
+        public static List<Data> listOfObjects = new List<Data>();
+
+        static void DecodeStringLineByRegexPattern(string singleLine, string regexPattern)
+        {
+            Match m = Regex.Match(singleLine, regexPattern);
+            Data data = new Data();
+
+            data.Integer = Int32.Parse(m.Groups["Integer"].Value);
+            data.String = m.Groups["String"].Value.Trim();
+            data.Date = DateTime.ParseExact(m.Groups["Date"].Value, "yyyy-mm-dd", CultureInfo.InvariantCulture);
+
+            listOfObjects.Add(data);
+
+            Console.WriteLine(singleLine);
+        }
+
+
         static void Main(string[] args)
         {
-            string url = @"..\File\mixedFormats.txt";
-            List<Data> listOfObjects = new List<Data>();
+            string url = @"C:\Users\user\Documents\Visual Studio 2017\Projects\Translators\FileLineRegexRecognizer\File\mixedFormats.txt";
 
-            string fixedSizeRegex = "";
-            string jsonRegex = "";
-            string csvRegex = "";
-            string xmlRegex = "";
+            //^(?<Integer>\d+) {1,31}(?<String>.*) {1,31}(?<Date>(([1-2]\d{3})\-([0]\d|[1][0-2])\-([[0-2]\d|[3][0-1]))){1,31}
+            string fixedSizeRegex = "^(?<Integer>\\d+) {1,31}(?<String>.*) {1,31}(?<Date>(([1-2]\\d{3})\\-([0]\\d|[1][0-2])\\-([[0-2]\\d|[3][0-1]))){1,31}";
+
+            //^ *"? *?(?<Integer>\d+) *"? *, *"? *(?<String>.*)"{0,1} *, *"? *(?<Date>(([1-2]\d{3})\-([0]\d|[1][0-2])\-([[0-2]\d|[3][0-1]))) *"? *,? *$
+            string jsonRegex = "^ *{ *\"Integer\": *(?<Integer>\\d+), *\"String\": *\"(?<String>.*)\", *\"Date\": *\"(?<Date>(([1-2]\\d{3})\\-([0]\\d|[1][0-2])\\-([[0-2]\\d|[3][0-1])))\" * }";
+
+            //^ *"? *?(?<Integer>\d+) *"? *, *"? *(?<String>.*)"{0,1} *, *"? *(?<Date>(([1-2]\d{3})\-([0]\d|[1][0-2])\-([[0-2]\d|[3][0-1]))) *"? *,? *$
+            string csvRegex = "^ *\"? *?(?<Integer>\\d+) *\"? *, *\"? *(?<String>.*)\"{0,1} *, *\"? *(?<Date>(([1-2]\\d{3})\\-([0]\\d|[1][0-2])\\-([[0-2]\\d|[3][0-1]))) *\"? *,? *";
+
+            //^ *<root> *<Integer>(?<Integer>\d+)<\/Integer> *<String>(?<String>.*)<\/String> *<Date>(?<Date>(([1-2]\d{3})\-([0]\d|[1][0-2])\-([[0-2]\d|[3][0-1])))<\/Date> *<\/root> *$
+            string xmlRegex = "^ *<root> *<Integer>(?<Integer>\\d+)<\\/Integer> *<String>(?<String>.*)<\\/String> *<Date>(?<Date>(([1-2]\\d{3})\\-([0]\\d|[1][0-2])\\-([[0-2]\\d|[3][0-1])))<\\/Date> *<\\/root> *$";
 
             using (StreamReader sr = new StreamReader(url))
             {
@@ -24,65 +48,29 @@ namespace FileLineRegexRecognizer
 
                 while ((singleLine = sr.ReadLine()) != null)
                 {
-                    // fixedsize
+                    //fixedsize 32characters per field
                     if (Regex.IsMatch(singleLine, fixedSizeRegex))
                     {
-                        Match m = Regex.Match(singleLine, fixedSizeRegex);
-                        Data data = new Data();
-
-                        data.Integer = Int32.Parse(m.Groups["integer"].Value);
-                        data.String = m.Groups["string"].Value.Trim();
-                        data.Date = DateTime.Parse(m.Groups["date"].Value, "yyyy-mm-dd", Culture....);
-
-                        listOfObjects.Add(data);
-
-                        Console.WriteLine(singleLine);
+                        DecodeStringLineByRegexPattern(singleLine, fixedSizeRegex);
                     }
 
                     //json
-                    //if (Regex.IsMatch(singleLine, jsonRegex))
-                    //{
-                    //    Match m = Regex.Match(singleLine, jsonRegex);
-                    //    Data data = new Data();
+                    if (Regex.IsMatch(singleLine, jsonRegex))
+                    {
+                        DecodeStringLineByRegexPattern(singleLine, jsonRegex);
+                    }
 
-                    //    data.Integer = Int32.Parse(m.Groups["integer"].Value);
-                    //    data.String = m.Groups["string"].Value.Trim();
-                    //    data.Date = DateTime.Parse(m.Groups["date"].Value, "yyyy-mm-dd", Culture....);
+                    //xml
+                    if (Regex.IsMatch(singleLine, xmlRegex))
+                    {
+                        DecodeStringLineByRegexPattern(singleLine, xmlRegex);
+                    }
 
-                    //    listOfObjects.Add(data);
-
-                    //    Console.WriteLine(singleLine);
-                    //}
-
-                    // xml
-                    //if (Regex.IsMatch(singleLine, xmlRegex))
-                    //{
-                    //    Match m = Regex.Match(singleLine, xmlRegex);
-                    //    Data data = new Data();
-
-                    //    data.Integer = Int32.Parse(m.Groups["integer"].Value);
-                    //    data.String = m.Groups["string"].Value.Trim();
-                    //    data.Date = DateTime.Parse(m.Groups["date"].Value, "yyyy-mm-dd", Culture....);
-
-                    //    listOfObjects.Add(data);
-
-                    //    Console.WriteLine(singleLine);
-                    //}
-
-                    // csv
-                    //if (Regex.IsMatch(singleLine, csvRegex))
-                    //{
-                    //    Match m = Regex.Match(singleLine, csvRegex);
-                    //    Data data = new Data();
-
-                    //    data.Integer = Int32.Parse(m.Groups["integer"].Value);
-                    //    data.String = m.Groups["string"].Value.Trim();
-                    //    data.Date = DateTime.Parse(m.Groups["date"].Value, "yyyy-mm-dd", Culture....);
-
-                    //    listOfObjects.Add(data);
-
-                    //    Console.WriteLine(singleLine);
-                    //}
+                    //csv
+                    if (Regex.IsMatch(singleLine, csvRegex))
+                    {
+                        DecodeStringLineByRegexPattern(singleLine, csvRegex);
+                    }
 
                 }
 
